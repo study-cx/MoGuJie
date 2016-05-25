caac66205c479447dce84f25e1462807144c5495
pengle1229 <2249157081@qq.com>
2016-05-25 21:17:15 +0800

    菜菜

--- MoGuJie/src/test/java/com/yc/vote/test/DBConnTest.java
+++ MoGuJie/src/test/java/com/yc/vote/test/DBConnTest.java
@@ -21,7 +21,7 @@ public class DBConnTest {
 	public void testConn() {
 		Connection con = sqlSessionFactory.openSession().getConnection();
 		assertNotNull("数据库连接失败!!", con);
-		assertNotNull("数据库连接失败!!", con);
+		
 	}
 
 }
