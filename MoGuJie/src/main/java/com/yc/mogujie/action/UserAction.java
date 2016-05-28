69e594083aaac156eb9aa59678b5474fa6eb74a1
pengle1229 <2249157081@qq.com>
2016-05-28 09:09:50 +0800

    菜菜

--- MoGuJie/src/main/java/com/yc/mogujie/action/UserAction.java
+++ MoGuJie/src/main/java/com/yc/mogujie/action/UserAction.java
@@ -1,7 +1,14 @@
 package com.yc.mogujie.action;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -9,28 +16,57 @@ import com.opensymphony.xwork2.ModelDriven;
 import com.yc.mogujie.entity.UserInfo;
 import com.yc.mogujie.service.UserInfoService;
 import com.yc.mogujie.util.MogujieData;
+import com.yc.mogujie.util.VerifyCode;
 
 @Controller("userAction")
 public class UserAction implements ModelDriven<UserInfo>{
 	private UserInfo userInfo;
+	private VerifyCode vc=new VerifyCode();
+	private String email;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Autowired
+	private JavaMailSender javaMailSender;
 	@Autowired
 	private UserInfoService userInfoService;
 	
 	public String login(){
 		LogManager.getLogger().debug(userInfo);
 		UserInfo us=userInfoService.login(userInfo);
-		ActionContext.getContext().getSession().put(MogujieData.LOGIN_USER, us);
-		return "login";
+		if(us!=null){
+			ActionContext.getContext().getSession().put(MogujieData.LOGIN_USER, us);
+			return "login";
+		}
+		return "false";
 	}
 	
 	
 	public String register(){
-		LogManager.getLogger().debug(userInfo);
-		int result=userInfoService.userRegister(userInfo);
+		SimpleMailMessage smm=new SimpleMailMessage();
+		smm.setFrom("studymail_test@163.com");
+		smm.setTo(userInfo.getUsemail());
+		smm.setSubject("验证码");
+		String uuid=vc.code();
+		ActionContext.getContext().getSession().put("userInfo",userInfo);
+		ActionContext.getContext().getSession().put(MogujieData.CODE, uuid);
+		smm.setText(uuid);
+		javaMailSender.send(smm);
+		return "tiaozhuan";
 		
-		return  "register";
 	}
-
+	public String registers(){
+		String jiaoyan=(String) ActionContext.getContext().getSession().get(MogujieData.CODE);
+		if(jiaoyan.equals(email)){
+			System.out.println(userInfo);
+			int result=userInfoService.userRegister(userInfo);
+			if(result>0){
+				return  "register";
+			}
+		}
+		return  "false";
+	}
 	@Override
 	public UserInfo getModel() {
 		userInfo=new UserInfo();
