3ffce8dc23b45f9b1cfeeeb154da0fc5bead25cf
Hi-story <1425449856@qq.com>
2016-06-07 19:53:27 +0800

    hms

--- MoGuJie/src/main/java/com/yc/mogujie/mapper/UserOrderMapper.java
+++ MoGuJie/src/main/java/com/yc/mogujie/mapper/UserOrderMapper.java
@@ -1,9 +1,7 @@
 package com.yc.mogujie.mapper;
 
-import com.yc.mogujie.entity.UserOrder;
 import com.yc.mogujie.entity.UserOrderBean;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface UserOrderMapper {
