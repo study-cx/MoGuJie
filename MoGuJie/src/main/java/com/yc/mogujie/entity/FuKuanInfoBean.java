62f955bb546c8eca288121673280170833ddd5c1
pengle1229 <2249157081@qq.com>
2016-06-07 21:42:27 +0800

    cc

---
+++ MoGuJie/src/main/java/com/yc/mogujie/entity/FuKuanInfoBean.java
@@ -0,0 +1,63 @@
+package com.yc.mogujie.entity;
+
+public class FuKuanInfoBean {
+	private String proname;
+	private int price;
+	private String color;
+	private String size;
+	private int number;
+	private int proid;
+	private String prophoto;
+	
+	public String getProphoto() {
+		return prophoto;
+	}
+	public void setProphoto(String prophoto) {
+		this.prophoto = prophoto;
+	}
+	public String getProname() {
+		return proname;
+	}
+	public void setProname(String proname) {
+		this.proname = proname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getProid() {
+		return proid;
+	}
+	public void setProid(int proid) {
+		this.proid = proid;
+	}
+	@Override
+	public String toString() {
+		return "FuKuanInfoBean [proname=" + proname + ", price=" + price
+				+ ", color=" + color + ", size=" + size + ", number=" + number
+				+ ", proid=" + proid + ", prophoto=" + prophoto + "]";
+	}
+	
+	
+	
+}
